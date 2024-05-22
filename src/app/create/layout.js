29e496258fbc059f63277d46dfca4f5026f1fffb
C-DYoung <rbrbdk12@gmail.com>
2024-05-22 22:34:54 +0900

    feat : 글 생성하기 doc : creat/laout.js 파일 삭제

--- src/app/create/layout.js
+++
@@ -1,9 +0,0 @@
-export default function Layout(props) {
-    // props 가 필수적으로 기입되어져야 함.
-    return(
-        <from>
-            <h2>CREATE</h2>
-            {props.children}
-        </from>
-    )
-}
--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,5 +1,44 @@
+"use client"
+
+// next/router -> next12 버전에서 사용하는 page router 방식
+// import { useRouter } from "next/router";
+// next13버전은 m router 를 사용해서 router가 아닌 navigation 에서 가져와야함
+import { useRouter } from "next/navigation";
+
 export default function Create() {
+    const router = useRouter();
     return(
-        <>Create! page!! </>
+        <form onSubmit={(e)=>{
+            // onSubmit 의 기본 동작을 막음. 
+            e.preventDefault(); 
+            const title = e.target.title.value;
+            const body = e.target.body.value;
+            // server로 데이터를 보내서 저장하는 것이라 options 필요
+            const options ={
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({title, body})
+            }
+            fetch(`http://localhost:9999/topics`, options)
+                .then(res=>res.json())
+                .then(result=>{
+                    console.log(result);
+                    const lastid = result.id;
+                    // 리디렉션
+                    router.push(`/read/${lastid}`);
+                })
+        }}>
+            <p>
+                <input type="text" name="title" placeholder="title" />
+            </p>
+            <p>
+                <textarea name="body" placeholder="body"></textarea>
+            </p>
+            <p>
+                <input type="submit" value="creat" />
+            </p>
+        </form>
     )
 }
