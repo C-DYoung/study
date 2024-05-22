c8f460eae17a02616e71bc8c9c3ed8ab50ca1b1b
C-DYoung <rbrbdk12@gmail.com>
2024-05-22 17:52:43 +0900

    feat : 게시글 읽기 기능 추가

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,8 +1,12 @@
-export default function Read(props) {
+// 데이터를 읽고 출력 => 사용자와 상호작용 x -> 서버 컴퍼넌트
+export default async function Read(props) {
+    const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+    const topic = await resp.json();
     return(
         <>
-            <h2>READ ! </h2>
-            parmters : {props.params.id}
+            <h2> {topic.title} </h2>
+            {topic.body}
+            
         </>
     )
 }
